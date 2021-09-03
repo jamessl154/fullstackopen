e53d7f2d679adeef75bad4299ccca50a808d2a7a
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-09-03 14:53:24 +0100

    part1 anecdotes step2 done

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -14,14 +14,28 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
+
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Array
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
+  // use a lazy initializer for the votes state
+  const [votes, setVotes] = useState(() => new Array(anecdotes.length).fill(0))
   
-  // When this function expression is called a random index
-  // corresponding to the anecdotes array is generated
+  // generates a random index corresponding to the anecdotes array
   const anecdotesRandom = () => Math.round(Math.random()*(anecdotes.length-1))
+
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+  // addVote is a function expression that modifies the votes state with setVotes
+  // When it's called maps the votes array, where x is an element in the array and y is the index.
+  // It only increments the index of the votes array that is the same value as the state, selected,
+  // which is the anecdote rendered on the page. ".map" makes a copy of the array, applies the
+  // mapping function, and returns the new array which maintains the array between states
+  const addVote = () => setVotes(votes.map((x, y) => (y === selected) ? x + 1 : x))
 
   return (
     <div>
       <h2>{anecdotes[selected]}</h2>
+      <h4>This anecdote has {votes[selected]} votes</h4>
+      <Button text="Vote" onClick={addVote} />
       <Button text="Next Anecdote" onClick={() => setSelected(anecdotesRandom)} />
     </div>
   )
