f7fcd9082b278392d72f2a2710f1b92181243c4b
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-09-03 08:42:24 +0100

    part1 unicafe step2 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,19 +10,38 @@ const Button = ({addReview, ...props}) => {
   )
 }
 
+const Stats = (props) => {
+
+  const averageScore = (props.score / props.all).toFixed(2)
+  
+  const percentPositive = (props.good * 100 / props.all).toFixed(2)
+
+  return (
+    <div>
+      <h2>Total ratings: {props.all}</h2>
+      <h2>Average score: {isNaN(averageScore) ? 0 : (averageScore)}</h2>
+      <h2>Percentage of positive ratings: {isNaN(percentPositive) ? 0 : (percentPositive)} %</h2>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   // const [rating, setRating] = useState('')
-  
+
+  const all = good + neutral + bad
+  const score = good - bad
+
   return (
     <>
       <h1>Rate your experience!</h1>
-      <Button rating="good" counter={good} addReview={() => setGood(good + 1)} />
-      <Button rating="neutral" counter={neutral} addReview={() => setNeutral(neutral + 1)} />
-      <Button rating="bad" counter={bad} addReview={() => setBad(bad + 1)} />
+      <Button rating="good " counter={good} addReview={() => setGood(good + 1)} />
+      <Button rating="neutral " counter={neutral} addReview={() => setNeutral(neutral + 1)} />
+      <Button rating="bad " counter={bad} addReview={() => setBad(bad + 1)} />
+      <Stats good={good} neutral={neutral} bad={bad} all={all} score={score} />
     </>
   )
 }
