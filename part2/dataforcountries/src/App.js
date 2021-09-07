6ad8add604b03e9e4d1d16af45f34bf2490f1cbe
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-09-07 17:22:58 +0100

    part 2 dataforcountries working step1 in progress

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,27 +1,75 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
+
+
+// const SingleCountry = (props) => {
+  
+//   const lang = props.languages.map((x) => <li>{x.name}</li>)
+  
+//   return (
+//     <div>
+//       capital: {props.capital}
+//       population: {props.population}
+//       <h2>languages</h2>
+//       <ul>
+//         {lang}
+//       </ul>
+//     </div>
+//   )
+// }
 
 const App = () => {
   const [countries, setCountries] = useState('')
   const [search, setSearch] = useState('')
 
-  console.log(countries)
+  const atlas = useRef()
+
   useEffect(() => {
     axios
     .get("https://restcountries.eu/rest/v2/all")
     .then(x => {
-      setCountries(x.data)
+      atlas.current = x.data
     })
   }, [])
 
+  useEffect(() => {
+    if (atlas.current !== undefined) {
+      setCountries(atlas.current.filter((x) =>
+        x.name.slice(0, search.length).toUpperCase()
+        ===
+        search.toUpperCase()
+        )
+      )
+    }
+  }, [search])
+
+  let countryList;
+  console.log(countries)
+  if (countries.length === 0) {
+    countryList = <p>No countries</p>
+  }
+  else if (countries.length > 10) {
+    countryList = <p>Too many countries</p>
+  }
+  else if (countries.length === 1) {
+    countryList = countries.map((x) => <p key={x.name}>{x.name}</p>)
+  }
+  else {
+    countryList = countries.map((x) => <p key={x.name}>{x.name}</p>)
+  }
+
+  // <SingleCountry key={x.name} country={x} />
+
   return (
     <div>
-      <h2>Countries:</h2>
+      <h2>Find a Country:</h2>
     
       <input 
         onChange={(e) => setSearch(e.target.value)}
         value={search}
       />
+
+      <div>{countryList}</div>
 
     </div>
   )
