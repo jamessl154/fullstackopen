28d105cfcb3c88835530d1cccf79085424289975
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-09-07 15:34:46 +0100

    part2 phonebook step6 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
@@ -40,6 +36,14 @@ const App = () => {
     )
   }
   , [filter, persons])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(x => {
+        setPersons(x.data)
+      })
+  }, [])
 
   const submitForm = (event) => {
     
@@ -77,7 +81,7 @@ const App = () => {
   let displayList = persons
   // When search is not empty, that means there are matches between the filter
   // input and the phonebook (persons array) which are stored in search state
-  if (search !== 0) {
+  if (search.length !== 0) {
     displayList = search
   }
 
