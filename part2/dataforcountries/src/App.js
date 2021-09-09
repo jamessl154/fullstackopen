5ae691cae3bffe3996c813b238b2e795beccb9f0
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-09-09 09:33:42 +0100

    part2 dataforcountries step3 working needs refactoring

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 
-const OneCountry = ({ props }) => {
-  // https://www.freecodecamp.org/news/array-and-object-destructuring-in-javascript/
-  // Object destructuring, props is the key here
+// https://www.freecodecamp.org/news/array-and-object-destructuring-in-javascript/
+// Object destructuring, props is a key here
+const OneCountry = ({ props, weather }) => {
   return (
     <div>
       <h2>{props.name}</h2>
@@ -14,6 +14,12 @@ const OneCountry = ({ props }) => {
         {props.languages.map((x) => <li key={x.name}>{x.name}</li>)}
       </ul>
       <img style={{ maxWidth: 150 }} src={props.flag} alt={props.name} />
+      <h2>Weather in {props.capital}:</h2>
+      {weather.weather_icons.map((x) => <img key={x} src={x} alt="Drawing of the current weather" />)}
+      {weather.weather_descriptions.map((x) => <p key={x} ><b>{x}</b></p>)}
+      <p><b>Temperature:</b> {weather.temperature}° C</p>
+      <p><b>Wind:</b> {weather.wind_speed} mph</p>
+      <p><b>Direction:</b> {weather.wind_dir}</p>
     </div>
   )
 }
@@ -21,9 +27,12 @@ const OneCountry = ({ props }) => {
 const App = () => {
   const [countries, setCountries] = useState('')
   const [search, setSearch] = useState('')
-
+  const [weather, setWeather] = useState('')
+  
+  // since we want to keep a reference to all of the 250 countries, useRef
   const atlas = useRef()
 
+  // This runs once on page load
   useEffect(() => {
     axios
     .get("https://restcountries.eu/rest/v2/all")
@@ -32,6 +41,7 @@ const App = () => {
     })
   }, [])
 
+  // our filtering input, search, sets countries where matches are found in atlas
   useEffect(() => {
     if (atlas.current !== undefined) {
       setCountries(atlas.current.filter((x) =>
@@ -43,21 +53,43 @@ const App = () => {
     }
   }, [search])
 
+  // https://egghead.io/lessons/react-make-http-requests-with-react-e06e2e96
+  // need to use a state to trigger a rerender and store weather
+  useEffect(() => {
+    if (countries.length === 1) {
+      let params = {
+        access_key: process.env.REACT_APP_API_KEY,
+        query: countries[0].capital
+      }
+      axios
+      .get('http://api.weatherstack.com/current', {params})
+      .then(response => {
+        setWeather(response.data)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+    }
+  }, [countries])
+
   // setOne takes name of 1 country as argument, x, then sets the countries state
   // to be that country
   const setOne = (x) => setCountries(countries.filter((y) => y.name === x))
 
   let countryList;
 
-  if (countries.length === 0) {
+  if (search.length === 0) {
+    countryList = <p>Type the name here</p>
+  }
+  else if (countries.length === 0) {
     countryList = <p>No countries</p>
   }
   else if (countries.length > 10) {
     countryList = <p>Too many countries</p>
   }
-  else if (countries.length === 1) {
+  else if (countries.length === 1 && weather !== '') {
     countryList = countries.map((x) => 
-      <OneCountry key={x.name} props={x} />
+      <OneCountry key={x.name} props={x} weather={weather.current} />
     )
   }
   else {
