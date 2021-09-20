5ab7aeb7a0ad35aafa4692312d8a507cb1108d23
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-09-20 11:12:44 +0100

    part4 bloglist step2 done. Refactored application into modules/directories. app.js is the main structure of the application

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,23 @@
+// https://expressjs.com/en/guide/routing.html
+// https://expressjs.com/en/4x/api.html#router
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = blogsRouter
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,51 +1,10 @@
+const app = require('./app') // the actual Express application
 const http = require('http')
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const url = process.env.MONGODB_URI
-
-mongoose.connect(url)
-
-app.use(cors())
-app.use(express.json())
-
-morgan.token('body', function(request) {
-  return JSON.stringify(request.body, null, 2)
-})
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
