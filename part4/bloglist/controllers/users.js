9a6cc6dd853ddc78016a977c94395ede7b8eefac
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-09-26 14:40:40 +0100

    Redesigned users/api post route after failing manual test in development mode

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -7,26 +7,28 @@ usersRouter.post('/', async (request, response, next) => {
   const body = request.body
 
   // Regex checker for password strength before the password is hashed
-  if(!helper.strongPassword(body.password)) {
+  const testPass = helper.strongPassword(body.password)
+
+  if(!testPass) {
     response.status(400).json(
       'Password must contain at least 1 upper case, ' +
       '1 lower case, 1 numeric and 1 symbol character')
-  }
+  } else {
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+    })
 
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash,
-  })
-
-  try {
-    const savedUser = await user.save()
-    response.json(savedUser)
-  } catch(exception) {
-    next(exception)
+    try {
+      const savedUser = await user.save()
+      response.json(savedUser)
+    } catch(exception) {
+      next(exception)
+    }
   }
 })
 
