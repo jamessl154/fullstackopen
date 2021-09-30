81c1889dcf507470109f0d75cbd20345195e12be
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-09-30 13:26:11 +0100

    part5 bloglist-frontend step1 done. Added loginForm (with username and password state) and blogDisplay that conditionally render depending on the result of the form onSubmit: handleLogin via loginService that changes the user state from null to the response.data from the backend server which is checking if that user/password combination exist and match in the DB

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,22 +1,82 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('Logging in with...', username, password)
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+
+  const loginForm = () => {
+    return (
+      <div>
+        <h2>Log in to the Application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            Username{' '}
+              <input
+                type="text"
+                value={username}
+                name="Username"
+                onChange={({ target }) => setUsername(target.value)}
+              />
+          </div>
+          <div>
+            Password{' '}
+              <input
+                type="password"
+                value={password}
+                name="Password"
+                onChange={({ target }) => setPassword(target.value)}
+              />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    )
+  }
+
+  const blogDisplay = () => {
+    return (
+      <div>
+        <h2>Blogs</h2>
+        <p>{user.username} is logged in</p>
+        {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      { user === null 
+        ? loginForm()
+        : blogDisplay()
+      }
     </div>
   )
 }
