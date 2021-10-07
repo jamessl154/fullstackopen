4d9c4ea44b127e5a9c5bb7057c88fe8f6ad6b227
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-10-07 09:34:40 +0100

    collected all tests in describe blocks

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -3,26 +3,29 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('Blog component renders a blog`s title and author,' +
+describe('On first render', () => {
+
+  test('Blog component renders a blog`s title and author,' +
   ' but not url or likes by default', () => {
 
-  const blog = {
-    title: 'testTitle',
-    author: 'testAuthor',
-    url: 'testUrl',
-    likes: 10
-  }
+    const blog = {
+      title: 'testTitle',
+      author: 'testAuthor',
+      url: 'testUrl',
+      likes: 10
+    }
 
-  const component = render(
-    <Blog blog={blog} />
-  )
+    const component = render(
+      <Blog blog={blog} />
+    )
 
-  const foundBlog = component.container.querySelector('.blog')
+    const foundBlog = component.container.querySelector('.blog')
 
-  expect(foundBlog).toHaveTextContent('testTitle')
-  expect(foundBlog).toHaveTextContent('testAuthor')
-  expect(foundBlog).not.toHaveTextContent('testUrl')
-  expect(foundBlog).not.toHaveTextContent('Likes')
+    expect(foundBlog).toHaveTextContent('testTitle')
+    expect(foundBlog).toHaveTextContent('testAuthor')
+    expect(foundBlog).not.toHaveTextContent('testUrl')
+    expect(foundBlog).not.toHaveTextContent('Likes')
+  })
 })
 
 describe('When View button is clicked for a blog', () => {
