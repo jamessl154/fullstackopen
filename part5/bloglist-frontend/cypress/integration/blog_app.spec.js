10b2bae735fecdf751f739587d4626aa8f66c6e1
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-10-08 15:25:24 +0100

    added a testingRouter when the backend is run in test mode, a post request will clear the user and blog databases. The backend is already configured to use the test DB with TEST_MONGODB_URI when receiving requests. added test to check the application diplays the login form by default

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,10 +1,11 @@
-describe('Blog app', function() {
+// https://docs.cypress.io/guides/references/best-practices#Setting-a-global-baseUrl
+// https://docs.cypress.io/guides/references/best-practices#Organizing-Tests-Logging-In-Controlling-State
+describe('On visit to the blog app,', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
   })
-
-  it('Login form is shown', function() {
-    // ...
+  it('the login form is shown', function() {
+    cy.get('[data-cy=login]').contains('Login')
   })
 })
--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -6,7 +6,7 @@ const LoginForm = ({ username, password, handleLogin,
   return (
     <div>
       <h2>Log in to the Application</h2>
-      <form onSubmit={handleLogin}>
+      <form data-cy='login' onSubmit={handleLogin}>
         <div className='container'>
           <label htmlFor='username'>Username:</label>
           <input
