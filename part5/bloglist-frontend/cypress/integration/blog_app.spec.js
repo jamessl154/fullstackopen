faab94e3f2f60aa55d5a8ae05fe3467852dd860d
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-10-09 08:31:17 +0100

    added custom cypress commands login and createBlog that facilitate bypassing the UI. added test that proves a logged in user can create a new blog

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -3,6 +3,7 @@
 describe('On visit to the blog app,', function() {
 
   beforeEach(function() {
+    // reset the DB
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
   })
@@ -15,9 +16,8 @@ describe('On visit to the blog app,', function() {
   describe('When logging in,',function() {
 
     beforeEach(function() {
-      // create user, bypass UI because we currently have no UI
-      // to register new users
-      // test login flow once here
+      // create user, bypass UI because we currently have no UI to register new users
+      // test manual login flow once here
       const user = {
         name: 'Matti Luukkainen',
         username: 'mluukkai',
@@ -25,7 +25,6 @@ describe('On visit to the blog app,', function() {
       }
 
       cy.request('POST', 'http://localhost:3003/api/users/', user)
-
     })
 
     it('succeeds with correct credentials', function() {
@@ -42,5 +41,54 @@ describe('On visit to the blog app,', function() {
       cy.get('.notification').contains('Wrong username or password.')
       cy.get('.notification').should('have.css', 'color', 'rgb(255, 0, 0)')
     })
+
+    describe('When logged in', function() {
+      beforeEach(function() {
+        // log in user
+        cy.login({ username: 'mluukkai', password: 'S1!lainen' })
+      })
+
+      it('A blog can be created', function() {
+
+        const blog = {
+          title: 'cypressTestTitle',
+          author: 'cypressTestAuthor',
+          url: 'cypressTestUrl'
+        }
+
+        cy.createBlog(blog)
+        cy.visit('http://localhost:3000')
+        cy.get('.bloglist')
+          .contains('cypressTestTitle')
+          .contains('cypressTestAuthor')
+      })
+    })
   })
+})
+
+// https://docs.cypress.io/api/cypress-api/custom-commands#Syntax
+
+Cypress.Commands.add('login', ({ username, password }) => {
+
+  cy.request('POST', 'http://localhost:3003/api/login', {
+    username, password
+  }).then(({ body }) => {
+    localStorage.setItem('loggedInBloglistUser', JSON.stringify(body))
+  })
+
+  cy.visit('http://localhost:3000')
+})
+
+Cypress.Commands.add('createBlog', (blog) => {
+
+  cy.request({
+    url: 'http://localhost:3003/api/blogs',
+    method: 'POST',
+    body: blog,
+    headers: {
+      'Authorization': `bearer ${JSON.parse(localStorage.getItem('loggedInBloglistUser')).token}`
+    }
+  })
+
+  cy.visit('http://localhost:3000')
 })
