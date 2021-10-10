c13993f64debfbd53a09a7fc00af1f509fc08587
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-10-10 10:59:12 +0100

    implemented the reducer and tests for it

--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -9,7 +9,6 @@ describe('unicafe reducer', () => {
   }
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
     const action = {
       type: 'DO_NOTHING'
     }
@@ -28,6 +27,49 @@ describe('unicafe reducer', () => {
     const newState = counterReducer(state, action)
     expect(newState).toEqual({
       good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('good is incremented twice', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const state = {
+      good: 9,
+      ok: 5,
+      bad: 2
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+
+    deepFreeze(newState)
+    const thirdState = counterReducer(newState, action)
+
+    expect(thirdState).toEqual({
+      good: 11,
+      ok: 5,
+      bad: 2
+    })
+  })
+
+  test('zero is called', () => {
+    const action = {
+      type: 'ZERO'
+    }
+
+    const state = {
+      good: 10,
+      ok: 20,
+      bad: 30
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
       ok: 0,
       bad: 0
     })
