a75290ef030bf98545bf5f10ec54bdb9bb70b1ea
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-10-11 15:43:42 +0100

    implemented a temporary backend using a json server that stores posted anecdotes

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,15 +2,25 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { addAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification, removeNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdoteService'
 
 const AnecdoteForm = () => {
 
     const dispatch = useDispatch()
 
-    const createAnecdote = (event) => {
+    const createAnecdote = async (event) => {
+
         event.preventDefault()
-        dispatch(addAnecdote(event.target.anecdote.value))
-        dispatch(setNotification(`You added the anecdote "${event.target.anecdote.value}"`))
+
+        // first communicate with backend, the anecdote string 
+        // gets assigned id and votes 0
+        const newAnecdote = 
+        await anecdoteService
+            .createNew(event.target.anecdote.value)
+
+        // then with returned object dispatch actions to stores (update state)
+        dispatch(addAnecdote(newAnecdote))
+        dispatch(setNotification(`You added the anecdote "${newAnecdote}"`))
         setTimeout(() => dispatch(removeNotification()), 5000)
         event.target.anecdote.value = ''
     }
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,25 +1,4 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   // console.log('state now: ', state)
   // console.log('action', action)
   switch (action.type) {
@@ -32,11 +11,12 @@ const anecdoteReducer = (state = initialState, action) => {
       return newAnecdotes
     case 'NEW_ANECDOTE':
       // console.log(action.data)
-      let mappedNewAnecdote = action.anecdote.map(asObject)
-      return state.concat(mappedNewAnecdote)
+      return state.concat(action.anecdoteObject)
     case 'SORT_ANECDOTES':
       state.sort((a, b) => b.votes - a.votes)
       return state
+    case 'INITIALIZE_ANECDOTES':
+      return action.allAnecdotes
     default:
       return state
   }
@@ -49,16 +29,23 @@ export const addVote = (id) => {
   }
 }
 
-export const addAnecdote = (anecdote) => {
+export const addAnecdote = (anecdoteObject) => {
   return {
     type: 'NEW_ANECDOTE',
-    anecdote: [anecdote]
+    anecdoteObject
   }
 }
 
 export const sortAnecdotes = () => {
   return {
     type: 'SORT_ANECDOTES'
+  }
+}
+
+export const initializeAnecdotes = (allAnecdotes) => {
+  return {
+    type: 'INITIALIZE_ANECDOTES',
+    allAnecdotes
   }
 }
 
