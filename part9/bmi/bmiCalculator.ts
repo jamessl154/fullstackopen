745eb831dd12f69c65a1d9133847de690048b280
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-11-09 12:32:09 +0000

    initialized TypeScript project, made CLI program bmiCalculator.ts

---
+++ part9/bmi/bmiCalculator.ts
@@ -0,0 +1,27 @@
+const calculateBmi = (height: number, weight: number) => {
+
+    if (process.argv.length < 4 || process.argv.length > 4) {
+        throw new Error("Correct usage:\n" +
+        "npm run calculateBmi <height> <weight>\n" + 
+        "npm run calculateBmi 180 74\n" +
+        "height in centimetres, weight in kilograms")
+    }
+    
+    if (isNaN(Number(process.argv[3])) ||
+        isNaN(Number(process.argv[3]))) {
+        throw new Error("Invalid inputs, height and weight must be integer symbols")
+    }
+    
+    let bmi: number = weight / (height / 100) ** 2
+
+    if (bmi <= 18.4) console.log("Underweight")
+    else if (bmi > 18.4 && bmi < 25) console.log("Normal range")
+    else if (bmi >= 25 && bmi < 30) console.log("Overweight")
+    else if (bmi >= 30) console.log("Obese")
+}
+
+try {
+    calculateBmi(Number(process.argv[3]), Number(process.argv[4]))
+} catch (error: unknown) {
+    if (error instanceof Error) console.log(error.message)
+}
