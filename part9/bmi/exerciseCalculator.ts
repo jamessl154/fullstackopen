bbdefe8b19730dd729900420ec98126fb326b12c
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-11-11 12:08:03 +0000

    added exerciseApi endpoint that receives inputs from request.body and returns the result. refactored exerciseCalculator into cliExercises.ts

--- part9/bmi/exerciseCalculator.ts
+++ part9/bmi/exerciseCalculator.ts
@@ -8,11 +8,6 @@ interface exerciseReport {
     average: number;
 }
 
-interface exerciseInput {
-    target: number;
-    hoursPerDay: Array<number>;
-}
-
 const calculateExercises = (target: number, hoursPerDay: Array<number>): exerciseReport => {
 
     let rating = 1;
@@ -44,28 +39,4 @@ const calculateExercises = (target: number, hoursPerDay: Array<number>): exercis
     };
 };
 
-const parseArguments = (args: Array<string>): exerciseInput => {
-
-    if (args.length < 4) throw new Error("Correct usage:\n" +
-    "npm run calculateExercises <target hours> <hours day 1> <hours day 2> e.t.c...\n" + 
-    "npm run calculateExercises 2 1 0 2 4.5 0 3 1 0 4\n");
-
-    const hoursPerDay = [];
-
-    for (let i = 2; i < args.length; i++) {
-        if (isNaN(Number(args[i]))) throw new Error("Invalid inputs, all hours must be integer symbols");
-        if (i >= 3) hoursPerDay.push(Number(args[i]));
-    }
-
-    return {
-        target: Number(args[2]),
-        hoursPerDay
-    };
-};
-
-try {
-    const { target, hoursPerDay } = parseArguments(process.argv);
-    console.log(calculateExercises(target, hoursPerDay));
-} catch (error: unknown) {
-    if (error instanceof Error) console.log(error.message);
-}
+export { calculateExercises };
--- part9/bmi/index.ts
+++ part9/bmi/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { apiBmi } from './bmiCalculator';
+import { calculateExercises as exerciseApi } from './exerciseCalculator';
 const app = express();
+
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -13,6 +16,25 @@ app.get('/bmi', (req, res) => {
       Number(req.query.weight)
     )
   );
+});
+
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line
+  const { target, daily_exercises } = req.body;
+
+  // check the properties on request body exist
+  if (!target || !daily_exercises) res.send({ error: "parameters missing" });
+  
+  else if (
+    // checking that target is a number and that daily exercises an array of only numbers
+    isNaN(Number(target)) || !Array.isArray(daily_exercises) || !daily_exercises.every((x) => typeof x === "number")
+  ) res.send({ error: "malformatted parameters" });
+
+  else {
+    // eslint-disable-next-line
+    const result = exerciseApi(Number(target), daily_exercises);
+    res.send(result);
+  }
 });
 
 const PORT = 3003;
