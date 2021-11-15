057be9a497cffc44db7ea742b83fa95c1a4d0058
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-11-15 15:24:57 +0000

    fixed spacing

--- part9/half-stack/src/components/Part.tsx
+++ part9/half-stack/src/components/Part.tsx
@@ -12,57 +12,57 @@ const Part = ({ part }: { part: CoursePart }) => {
     };
 
     switch(part.type) {
-            case "normal":
-                return (
-                    <>
-                        <h3>{part.name}</h3>
-                        <ul>
-                            <li><b>Exercise Count:</b> {part.exerciseCount}</li>
-                            <li><b>Description: </b>{part.description}</li>
-                            <li><b>Type: </b>{part.type}</li>
-                        </ul>
-                    </>
-                )
-            case "groupProject":
-                return (
-                    <>
-                        <h3>{part.name}</h3>
-                        <ul>
-                            <li><b>Exercise Count:</b> {part.exerciseCount}</li>
-                            <li><b>Group Project Count: </b>{part.groupProjectCount}</li>
-                            <li><b>Type: </b>{part.type}</li>
-                        </ul>
-                    </>
-                )
-            case "submission":
-                return (
-                    <>
-                        <h3>{part.name}</h3>
-                        <ul>
-                            <li><b>Exercise Count:</b> {part.exerciseCount}</li>
-                            <li><b>Submission Link: </b>{part.exerciseSubmissionLink}</li>
-                            <li><b>Type: </b>{part.type}</li>
-                        </ul>
-                    </>
-                )
-            case "special":
-                return (
-                    <>
-                        <h3>{part.name}</h3>
-                        <ul>
-                            <li><b>Exercise Count:</b> {part.exerciseCount}</li>
-                            <li><b>Description: </b>{part.description}</li>
-                            <li><b>Type: </b>{part.type}</li>
-                            <li><b>Requirements: </b></li>
-                                <ul>{part.requirements.map((requirement) =>
-                                        <li key={requirement}>{requirement}</li>
-                                    )}
-                                </ul>
-                        </ul>
-                    </>
-                )
-            default:
-                return assertNever(part)
+        case "normal":
+            return (
+                <>
+                    <h3>{part.name}</h3>
+                    <ul>
+                        <li><b>Exercise Count:</b> {part.exerciseCount}</li>
+                        <li><b>Description: </b>{part.description}</li>
+                        <li><b>Type: </b>{part.type}</li>
+                    </ul>
+                </>
+            )
+        case "groupProject":
+            return (
+                <>
+                    <h3>{part.name}</h3>
+                    <ul>
+                        <li><b>Exercise Count:</b> {part.exerciseCount}</li>
+                        <li><b>Group Project Count: </b>{part.groupProjectCount}</li>
+                        <li><b>Type: </b>{part.type}</li>
+                    </ul>
+                </>
+            )
+        case "submission":
+            return (
+                <>
+                    <h3>{part.name}</h3>
+                    <ul>
+                        <li><b>Exercise Count:</b> {part.exerciseCount}</li>
+                        <li><b>Submission Link: </b>{part.exerciseSubmissionLink}</li>
+                        <li><b>Type: </b>{part.type}</li>
+                    </ul>
+                </>
+            )
+        case "special":
+            return (
+                <>
+                    <h3>{part.name}</h3>
+                    <ul>
+                        <li><b>Exercise Count:</b> {part.exerciseCount}</li>
+                        <li><b>Description: </b>{part.description}</li>
+                        <li><b>Type: </b>{part.type}</li>
+                        <li><b>Requirements: </b></li>
+                            <ul>{part.requirements.map((requirement) =>
+                                    <li key={requirement}>{requirement}</li>
+                                )}
+                            </ul>
+                    </ul>
+                </>
+            )
+        default:
+            return assertNever(part)
     }
 }
 
