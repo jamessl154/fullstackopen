423dfb9e16cd3d07923e11d2b124f401c7f256ce
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-02-09 16:12:16 +0000

    update README.md for part9 and added comments

--- part9/patientor/src/state/state.tsx
+++ part9/patientor/src/state/state.tsx
@@ -13,6 +13,8 @@ const initialState: State = {
   diagnoses: {}
 };
 
+// When the app starts, the context is initialized with these values. state is just the initialState and
+// dispatch is just a function that returns the initial state
 export const StateContext = createContext<[State, React.Dispatch<Action>]>([
   initialState,
   () => initialState
@@ -23,11 +25,17 @@ type StateProviderProps = {
   children: React.ReactElement;
 };
 
+// StateProvider takes props reducer and children. props.children is automatically created with the JSX:
+//   <StateProvider>
+//     {children}
+//   </StateProvider>
+// https://reactjs.org/docs/jsx-in-depth.html#children-in-jsx
 export const StateProvider: React.FC<StateProviderProps> = ({
   reducer,
   children
 }: StateProviderProps) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  // when the value of the context changes, the provider's descendants are re-rendered https://reactjs.org/docs/context.html#contextprovider
   return (
     <StateContext.Provider value={[state, dispatch]}>
       {children}
@@ -35,3 +43,6 @@ export const StateProvider: React.FC<StateProviderProps> = ({
   );
 };
 export const useStateValue = () => useContext(StateContext);
+// when we call useStateValue() we get the current [state, dispatch] from the StateContext.Provider
+// this allows deeply nested components to dispatch actions to the reducer to change state (value of the Context.provider)
+// and trigger re-renders with the new state
